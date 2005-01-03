a70be996bb859eef79b5f8038fa250f24dbf7e78
Luke Biddell <luke.biddell@gmail.com>
2005-01-03 11:49:36 +0000

    Use the splash screen in the about dialog as it looks cool.

--- src/gnomebaker.c
+++ src/gnomebaker.c
@@ -317,7 +317,7 @@ gnomebaker_on_about(GtkMenuItem * menuitem, gpointer user_data)
 	
 	GtkWidget* about = gnome_about_new(_("GnomeBaker"), VERSION, "LGPL", 
 		_("Simple CD Burning for Gnome"), authors, documenters, _(""), 
-		gtk_window_get_icon(GTK_WINDOW(glade_xml_get_widget(xml, widget_gnomebaker))));
+		gdk_pixbuf_new_from_file(PACKAGE_PIXMAPS_DIR"/splash_2.png", NULL));
 	
 	gtk_widget_show(about);	
 }
