fa62e6915a0fa911095c560124c4421ba61cb9b3
Luke Biddell <luke.biddell@gmail.com>
2005-01-16 11:58:57 +0000

    Add new select device dlg

---
+++ src/selectdevicedlg.c
@@ -0,0 +1,64 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * File: selectdevicedlg.h
+ * Created by: User luke biddell
+ * Created on: Sun Jan 16 00:42:55 2005
+ */
+
+
+#include "selectdevicedlg.h"
+#include "gbcommon.h"
+#include "devices.h"
+#include "preferences.h"
+
+
+GladeXML* selectdevicedlgdlg_xml = NULL;
+
+
+GtkWidget* 
+selectdevicedlg_new(void)
+{
+	GB_LOG_FUNC	
+	selectdevicedlgdlg_xml = glade_xml_new(glade_file, widget_select_device_dlg, NULL);
+	glade_xml_signal_autoconnect(selectdevicedlgdlg_xml);		
+	
+	GtkWidget *optmenWriteDev = glade_xml_get_widget(selectdevicedlgdlg_xml, widget_select_writer);
+	devices_populate_optionmenu(optmenWriteDev, GB_WRITER);
+	
+	return glade_xml_get_widget(selectdevicedlgdlg_xml, widget_select_device_dlg);	
+}
+
+
+void 
+selectdevicedlg_delete(GtkWidget* self)
+{
+	g_return_if_fail(NULL != self);
+	gtk_widget_destroy(self);
+	g_free(selectdevicedlgdlg_xml);
+	selectdevicedlgdlg_xml = NULL;
+}
+
+
+void 
+selectdevicedlg_on_ok_clicked(GtkButton* button, gpointer user_data)
+{
+	GB_LOG_FUNC	
+	g_return_if_fail(selectdevicedlgdlg_xml != NULL);
+	
+	GtkWidget* optmenWriteDev = glade_xml_get_widget(selectdevicedlgdlg_xml, widget_select_writer);
+	devices_save_optionmenu(GTK_OPTION_MENU(optmenWriteDev), GB_WRITER);
+}
---
+++ src/selectdevicedlg.h
@@ -0,0 +1,39 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * File: selectdevicedlg.h
+ * Created by: luke biddell
+ * Created on: Sun Jan 16 00:42:55 2005
+ */
+
+#ifndef _SELECTDEVICEDLG_H_
+#define _SELECTDEVICEDLG_H_
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+
+#include <gnome.h>
+#include <glade/glade.h>
+
+
+
+GtkWidget* selectdevicedlg_new(void);
+void selectdevicedlg_delete(GtkWidget* self);
+
+
+#endif	/*_SELECTDEVICEDLG_H_*/
