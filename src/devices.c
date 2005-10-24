6a7099231efb499acf77be8ed47fdfb8195039c9
Luke Biddell <luke.biddell@gmail.com>
2005-10-24 21:11:04 +0000

    Only prompt once for a disk as it drives people mad.

--- src/devices.c
+++ src/devices.c
@@ -779,7 +779,8 @@ devices_prompt_for_disk(GtkWindow* parent, const gchar* devicekey)
     gchar* devicename = devices_get_device_config(devicekey, GB_DEVICE_NAME_LABEL);
     gchar* message = g_strdup_printf(_("Please insert a disk into the %s"), devicename);    
     gint ret = GTK_RESPONSE_OK;
-    while(!devices_is_disk_inserted(devicekey) && (ret == GTK_RESPONSE_OK))
+    /*while(!devices_is_disk_inserted(devicekey) && (ret == GTK_RESPONSE_OK))*/
+    if(!devices_is_disk_inserted(devicekey))
     {
         devices_eject_disk(devicekey);
         ret = gnomebaker_show_msg_dlg(parent, GTK_MESSAGE_INFO, 
