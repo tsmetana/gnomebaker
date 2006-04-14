4d44440132b2b85c4adf937f39f7f8a107433016
Luke Biddell <luke.biddell@gmail.com>
2006-04-14 23:43:45 +0000

    add copyright

--- src/project.c
+++ src/project.c
@@ -1,3 +1,24 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+/*
+ * File: project.c
+ * Copyright: luke_biddell@yahoo.com
+ * Created on: Sun Mar  19 21:19:51 2006
+ */
+
 #include "project.h"
 #include "gbcommon.h"
 #include "gnomebaker.h"
