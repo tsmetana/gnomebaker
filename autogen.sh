47bd55e8decaae7abd5e5cc590e22a4e8db5a404
Mario Đanić <mario.danic@gmail.com>
2007-09-11 14:03:57 +0000

    Changed autogen forced versions

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@
 glib-gettextize -f -c
 intltoolize --copy --force --automake
 
-ACLOCAL=aclocal-1.9 AUTOMAKE=automake-1.9 autoreconf -v --install --force || exit 1
+ACLOCAL=aclocal AUTOMAKE=automake autoreconf -v --install --force || exit 1
 ./configure --enable-maintainer-mode "$@"
